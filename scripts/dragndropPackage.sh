d80894a4865dfe3479f09f848ebdad4f91462535
mongilyov <andrey1987697@gmail.com>
2023-03-26 18:50:37 +0300

    Fixed some scripts

--- scripts/dragndropPackage.sh
+++ scripts/dragndropPackage.sh
@@ -3,7 +3,7 @@ cd solver_application/build
 cmake -DCPACK_GENERATOR_DMG=ON ..
 make package
 make package_source
-mkdir artifact          
-mv solver_application/build/*.dmg artifact
-mv solver_application/build/*.zip artifact
-mv solver/application/build/*.tar.gz artifact
+mkdir artifacts          
+mv solver_application/build/*.dmg artifacts
+mv solver_application/build/*.zip artifacts
+mv solver/application/build/*.tar.gz artifacts
