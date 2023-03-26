1c270f1fd1a9d939ac7cda55e4f07ab07845edec
mongilyov <andrey1987697@gmail.com>
2023-03-26 21:08:32 +0300

    Added interruption in code

--- solver_application/sources/equation.cpp
+++ solver_application/sources/equation.cpp
@@ -6,6 +6,7 @@
 int main()
 {
     std::cout << "Quick interruption in code" << std::endl;
+    std::cout << "Quick interruption in code 2" << std::endl;
     float a = 0;
     float b = 0;
     float c = 0;
