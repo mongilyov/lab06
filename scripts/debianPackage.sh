64acd0a2b4cd9c25c8d1dea5039cb80608f5537c
mongilyov <andrey1987697@gmail.com>
2023-03-26 16:56:35 +0300

    test

--- scripts/debianPackage.sh
+++ scripts/debianPackage.sh
@@ -4,6 +4,8 @@ cmake -DCPACK_GENERATOR_DEB=ON ..
 make package
 make package_source
 mkdir artifacts          
+ls
+ls solver/application/build
 mv solver_application/build/*.deb artifacts
 mv solver_application/build/*.zip artifacts
 mv solver/application/build/*.tar.gz artifacts
